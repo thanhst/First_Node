699b07ac97437e3b4c5e5408ddcd3f92b40662be
thanhst <thanhxk2003@gmail.com>
2024-02-19 21:41:18 +0700

    Change little

--- Router/web.js
+++ Router/web.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 
-router.get('/XinChao',(req,res)=>{
+router.get('/HelloThanh',(req,res)=>{
     res.send('<h1>Hello Thanh</h1>')
 })
 
